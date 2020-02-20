25bf0aa5af0985075f75539e3698e4c7ad62cb58
15341a1205 <41323544+15341a1205@users.noreply.github.com>
2020-02-20 15:24:01 +0530

    Initial commit

---
+++ OopsDemo/Customer.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace OopsDemo
+{
+    class Customer
+    {
+        public string cid;
+        public string name, address;
+        public void get(int i, string cname, string add)
+        {
+            cid = i;
+            name = cname;
+            address = add;
+        }
+
+    }
+   
+    class Bankaccount
+    {
+        int accountnumber, balance;
+        Customer c;
+        int principle = 1000, rate = 3, time = 2;
+        private string accno;
+
+        public void bankDetail(int accno, int bal, Customer c)
+        {
+            accountnumber = accno;
+            balance = bal;
+            this.c = c;
+        }
+
+        public virtual double interestamount(double p,int t,double r)
+        {
+            double simpleinterest = (p * t * r) / 100;
+            return simpleinterest;
+        }
+
+        public void display()
+        {
+            Console.WriteLine("customer id:" + c.cid);
+            Console.WriteLine("customer name:" + c.name);
+            Console.WriteLine("customer address:" + c.address);
+            Console.WriteLine("account number:" + accno);
+            Console.WriteLine("account balance:" +balance);
+
+        }
+        
+    }
+
+    class FixedAccount:BankAccount
+    {
+        public override double interestamount(double p,int t,double r)
+        {
+            double s1 = (p * t * r) / 100;
+            Console.WriteLine("simpleinterest +3% additional:")
+                return s1;
+        }
+    }
+
+    
+
+    class BankProgram
+    {
+        static void main(string[] args)
+        {
+            Customer c2 = new Customer();
+            c2.get(5, "supraja", "sklm");
+            BankAccount b = new BankAccount;
+            b.bankDetail(2345, 4000, b2);
+            b.display();
+            Console.WriteLine(b.interestamount(1000, 2, 3));
+            FixedAccount f = new FixedAccount();
+            Console.WriteLine(f.interestamount(1000, 2, 3.03));
+            Console.ReadKey();
+        }
+    }
+
+}
---
+++ OopsDemo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OopsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* Employee emp=new Employee
+            {
+                EmpId=101,
+                Name="Anand",
+                Dept=new Department
+            }
+
+            Analyst al=new Analyst
+            {
+                EmpId=323,
+                Name="vijaya",
+
+            }*/
+
+        }
+
+      
+        
+            public static void demo()
+            {
+                //Demo d = new Demo();
+                Demo d1 = new Demo(12, 23);
+                Demo d2 = new Demo(23, 12);
+
+                Demo da = d1 + d2;
+                Demo ds = d1 - d2;
+
+                da.show();
+                ds.show();
+
+            }
+            Demo d1 = new Demo(12, 23);
+            Demo d2 = new Demo(23, 12);
+
+            Demo da = d1 + d2;
+            Demo ds = d1 - d2;
+
+            da.show();
+            ds.show();
+           
+
+        }
+
+
+    }
+}
